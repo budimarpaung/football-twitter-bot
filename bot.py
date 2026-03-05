db2ce5e7e8471a065b0db4780a71a0f066575f8e
budimarpaung <50257667+budimarpaung@users.noreply.github.com>
2026-03-05 20:34:29 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,73 @@
+import requests
+from PIL import Image, ImageDraw, ImageFont
+import tweepy
+import config
+
+def get_last_match(team):
+    url = f"{config.SPORTSDB_API}searchteams.php?t={team}"
+    r = requests.get(url).json()
+
+    team_id = r["teams"][0]["idTeam"]
+
+    url2 = f"{config.SPORTSDB_API}eventslast.php?id={team_id}"
+    match = requests.get(url2).json()["results"][0]
+
+    return match
+
+
+def create_result_image(match):
+
+    img = Image.new("RGB", (900, 500), (20,20,20))
+    draw = ImageDraw.Draw(img)
+
+    text = f"{match['strHomeTeam']} {match['intHomeScore']} - {match['intAwayScore']} {match['strAwayTeam']}"
+
+    draw.text((100,200), text, fill="white")
+
+    file = "result.png"
+    img.save(file)
+
+    return file
+
+
+def tweet_result(match):
+
+    client = tweepy.Client(
+        consumer_key=config.API_KEY,
+        consumer_secret=config.API_SECRET,
+        access_token=config.ACCESS_TOKEN,
+        access_token_secret=config.ACCESS_SECRET
+    )
+
+    text = f"FT: {match['strHomeTeam']} {match['intHomeScore']} - {match['intAwayScore']} {match['strAwayTeam']}"
+
+    image = create_result_image(match)
+
+    api = tweepy.API(
+        tweepy.OAuth1UserHandler(
+            config.API_KEY,
+            config.API_SECRET,
+            config.ACCESS_TOKEN,
+            config.ACCESS_SECRET
+        )
+    )
+
+    media = api.media_upload(image)
+
+    client.create_tweet(text=text, media_ids=[media.media_id])
+
+
+def main():
+
+    for team in config.TEAMS:
+
+        try:
+            match = get_last_match(team)
+            tweet_result(match)
+
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    main()
