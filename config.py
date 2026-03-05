1d13793daeb01d9c1472c2d890ea7794310b75b1
budimarpaung <50257667+budimarpaung@users.noreply.github.com>
2026-03-05 20:35:18 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# TheSportsDB API
+SPORTSDB_API = "https://www.thesportsdb.com/api/v1/json/3/"
+
+# Twitter API
+API_KEY = "ISI"
+API_SECRET = "ISI"
+ACCESS_TOKEN = "ISI"
+ACCESS_SECRET = "ISI"
+
+# club yang dipantau
+TEAMS = [
+"Liverpool",
+"Arsenal",
+"Manchester United",
+"Chelsea",
+"Manchester City",
+"Real Madrid",
+"Barcelona",
+"Juventus",
+"Inter",
+"Milan",
+"Bayern Munich",
+"Paris SG"
+]
